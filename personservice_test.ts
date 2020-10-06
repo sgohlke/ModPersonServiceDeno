8472d465307afde7568d996936d18fef85044bbd
stefangohlke <gohlke.stefan@yahoo.de>
2020-10-06 14:10:59 +0200

    Add PersonService and test

---
+++ personservice_test.ts
@@ -0,0 +1,30 @@
+import {
+  assertEquals, assertArrayContains
+} from 'https://deno.land/std@0.67.0/testing/asserts.ts';
+import { Person } from './personservice.ts';
+import { PersonService } from './personservice.ts';
+
+Deno.test("Create a test Person object", () => {
+  const person = new Person(1, 'McGulley', 'Mike', 43)
+  assertEquals(person.id, 1)
+  assertEquals(person.lastName, 'McGulley')
+  assertEquals(person.firstName, 'Mike')
+  assertEquals(person.age, 43)
+});
+
+Deno.test("All persons are returned", () => {
+  const personList = PersonService.getAllPersons()
+  assertEquals(personList.length, 3)
+  assertArrayContains<Person>(personList, [new Person(1, "Meier", "Hans", 40)])
+  assertArrayContains<Person>(personList, [new Person(2, "Mahler", "Sandra", 32)])
+  assertArrayContains<Person>(personList, [new Person(3, "Huber", "Franz", 56)])
+});
+
+Deno.test("Person with id 1 is returned", () => {
+  const person = PersonService.getPersonForId("1")
+  assertEquals(person?.id, 1)
+  assertEquals(person?.lastName, 'Meier')
+  assertEquals(person?.firstName, 'Hans')
+  assertEquals(person?.age, 40)
+});
+
