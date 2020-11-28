c552693350e6f8f98a0e87d879c7465f64a9314b
stefangohlke <gohlke.stefan@yahoo.de>
2020-11-28 08:05:50 +0100

    Cleanup module. Add missing return types

--- personservice.ts
+++ personservice.ts
@@ -1,14 +1,14 @@
 export class Person {
-  id: number;
-  lastName = "UNKNOWN";
-  firstName = "UNKNOWN";
-  age = 999;
+  id: number
+  lastName = "UNKNOWN"
+  firstName = "UNKNOWN"
+  age = 999
 
   constructor(id: number, lastName: string, firstName: string, age: number) {
-    this.id = id;
-    this.lastName = lastName;
-    this.firstName = firstName;
-    this.age = age;
+    this.id = id
+    this.lastName = lastName
+    this.firstName = firstName
+    this.age = age
   }
 }
 
@@ -16,14 +16,14 @@ const personList = new Map([
   ["1", new Person(1, "Meier", "Hans", 40)],
   ["2", new Person(2, "Mahler", "Sandra", 32)],
   ["3", new Person(3, "Huber", "Franz", 56)],
-]);
+])
 
 export class PersonService {
-  static getAllPersons() {
-    return Array.from(personList.values());
+  static getAllPersons(): Person[] {
+    return Array.from(personList.values())
   }
 
-  static getPersonForId(id: string) {
-    return personList.get(id);
+  static getPersonForId(id: string): Person | undefined {
+    return personList.get(id)
   }
 }
