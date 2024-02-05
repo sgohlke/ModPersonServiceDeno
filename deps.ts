fb17ae13ae6e6444fb476e6882c7c57b115423c8
sgohlke <gohlke.stefan@yahoo.de>
2024-02-05 22:12:28 +0100

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,4 @@
 export {
-   assertEquals,
-} from 'https://deno.land/std@0.212.0/assert/assert_equals.ts'
-
-export {
    assertArrayIncludes,
-} from 'https://deno.land/std@0.212.0/assert/assert_array_includes.ts'
+   assertEquals,
+} from 'https://raw.githubusercontent.com/sgohlke/deno-deps/main/deps.ts'
